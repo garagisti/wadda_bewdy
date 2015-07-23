fb1108d16afa9ba697d8723d8efe6b1b78f007a9
Rahul Bose <schu82@gmail.com>
2015-07-23 20:52:23 +1000

    Cleaned the front end

--- app/models/qly_result.rb
+++ app/models/qly_result.rb
@@ -3,8 +3,8 @@ class QlyResult < ActiveRecord::Base
   has_many :drivers
   validates_presence_of :round_id, :qly_result_1, :qly_result_2, :qly_result_3
 
-  def to_s
-    Driver.where(id: qly_result_1).first
+  def pole_position
+    Driver.find_by_id(qly_result_1)
   end
 
 end
--- app/models/race_result.rb
+++ app/models/race_result.rb
@@ -6,8 +6,8 @@ class RaceResult < ActiveRecord::Base
                         :race_result_6, :race_result_7, :race_result_8,
                         :race_result_9, :race_result_10
 
-  def to_s
-    Driver.where(id: race_result_1).first
+  def winner
+    Driver.find_by_id(race_result_1)
   end
 
 end
