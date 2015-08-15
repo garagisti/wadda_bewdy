f3e11887be08d22383b6cf0d26e185d055dbeb8c
Rahul Bose <schu82@gmail.com>
2015-08-15 11:45:10 +1000

    Added Qualy Tip validation tests

--- app/models/tip.rb
+++ app/models/tip.rb
@@ -5,18 +5,29 @@ class Tip < ActiveRecord::Base
 
 
   def valid_qly_tip?
+    # Assume tip is valid to begin with
+    validity = true
+
     # check there are 3 entries
     if qly_tip_1.nil? || qly_tip_2.nil? || qly_tip_3.nil? ||
        qly_tip_1.blank? || qly_tip_2.blank? || qly_tip_3.blank?
-       false
-    else
-      true
-    end
+       validity = false
 
     # check there are no duplicates
+    elsif qly_tip_1 == qly_tip_2 ||
+          qly_tip_1 == qly_tip_3 ||
+          qly_tip_2 == qly_tip_3
+      validity = false
+    end
+    validity
   end
 
   def valid_race_tip?
+    # Check there are 10 entries
+
+    # check there are no duplicates
+
+
   end
 
 end
--- spec/models/tip_spec.rb
+++ spec/models/tip_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'A Tip', type: :model do
 
   subject(:tip) {Tip.new}
 
-  context "when valid qualy tip are entered" do
+  context "when valid qualy tips are entered" do
 
     let(:qly_tips) {Tip.new(round_id: 1, qly_tip_1: 1, qly_tip_2: 2, qly_tip_3: 3)}
 
@@ -12,19 +12,35 @@ RSpec.describe 'A Tip', type: :model do
       expect(qly_tips.valid_qly_tip?).to be_truthy
     end
 
+    it "there should be no duplicates" do
+      expect(qly_tips.valid_qly_tip?).to be_truthy
+    end
+  end
+
+  context "when invalid qualy tips are entered" do
+
+    let(:qly_tips) {Tip.new(round_id: 1, qly_tip_1: 1, qly_tip_2: nil, qly_tip_3: '')}
+
     it "is invalid if there are not 3 entries" do
-      qly_tips[:qly_tip_3] = ""
       expect(qly_tips.valid_qly_tip?).to be_falsey
     end
 
-    # it "there should be no duplicates" do
-      # expect(qly_tips.valid?)
-    # end
+    it "is invalid if there are duplicate tips" do
+      qly_tips.qly_tip_1 = 2
+      qly_tips.qly_tip_2 = 2
+      qly_tips.qly_tip_3 = 2
+      expect(qly_tips.valid_qly_tip?).to be_falsey
+    end
   end
 
- context "when an invalid qualy tips are entered" do
+  context "when valid race tips are entered" do
 
-  it "duplicate entries are invalid"
-  it "less than 3 entries are invalid"
- end
+    let(:qly_tips) {Tip.new(round_id: 1, race_tip_1: 1, race_tip_2: 2, race_tip_3: 3,
+                            race_tip_4: 4, race_tip_5: 5, race_tip_6: 6, race_tip_7: 7,
+                            race_tip_8: 8, race_tip_9: 9, race_tip_10:10)}
+
+    it "should have 10 entries"
+    it "there should be no duplicates"
+  end
+
 end
